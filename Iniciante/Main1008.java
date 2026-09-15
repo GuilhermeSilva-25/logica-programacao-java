7980fafcb9a998098eac3b14f426fb9fb73d934d
Guilherme <guime.silva333@gmail.com>
2026-09-15 18:25:49 -0300

    feat: adicionar solução do beecrowd 1008

---
+++ Iniciante/Main1008.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main1008 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int numeroFuncionario = sc.nextInt();
+        int horasTrabalhadas = sc.nextInt();
+        double valorHora = sc.nextDouble();
+
+        double salario = horasTrabalhadas * valorHora;
+
+        System.out.printf("NUMBER = %d%nSALARY = U$ %.2f%n", numeroFuncionario, salario);
+        sc.close();
+    }
+}
